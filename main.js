b9fcd92f2f1e0ed43fc1fd1beb05d234314357d8
Carlos <carlos.viller98@gmail.com>
2020-09-12 13:50:06 -0300

    fix

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ $.getJSON("https://api.chucknorris.io/jokes/random",function (data) {
     document.querySelector('h2').textContent = data.value;
   }
 );
+
+
+$('button').click(function (e) { 
+  e.preventDefault();
+  $.getJSON("https://api.chucknorris.io/jokes/random",function (data) {
+    document.querySelector('h2').textContent = data.value;
+  }
+  );
+});
